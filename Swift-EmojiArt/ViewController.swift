3e26e17936057b0c3e32456c7ffe89feb13b1f7e
Jiacheng Sun <jakesun113@gmail.com>
2018-11-21 15:06:28 +1100

    Initial Commit

---
+++ Swift-EmojiArt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift-EmojiArt
+//
+//  Created by wry on 2018/11/21.
+//  Copyright © 2018年 jiacheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
